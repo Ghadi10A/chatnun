06870ba850bb657c9ace79ef7483cd3b90d4e03f
ghadi10 <72098445+ghadi00@users.noreply.github.com>
2023-06-02 18:45:01 +0100

    Update utils.py

--- myapp/utils.py
+++ myapp/utils.py
@@ -50,9 +50,8 @@ def train_and_save_model(ticker):
     # Retrieve the data for the specified ticker from Yahoo Finance
     data = yf.Ticker(ticker).history(period="max")
 
-    # Check if the data is empty
     if data.empty:
-        return None
+        return None, 'No data available'
 
     # Calculate the VWAP
     data['VWAP'] = (data['Close'] * data['Volume']).cumsum() / data['Volume'].cumsum()
@@ -90,6 +89,7 @@ def train_and_save_model(ticker):
     joblib.dump(scaler, scaler_file)
 
     return accuracy
+
 
 def predict_signal(ticker):
     model_file = os.path.join(settings.BASE_DIR, 'myapp', 'models', f'{ticker}_model.pkl')
