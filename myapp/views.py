435c5b939bd1208b86f10a528ccef4e2b409777b
ghadi10 <72098445+ghadi00@users.noreply.github.com>
2023-06-03 15:16:54 +0100

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -354,7 +354,7 @@ def show_profile(request, username):
         profile = user.profile
     except Profile.DoesNotExist:
         profile = Profile.objects.create(user=user)
-    posts = Post.objects.filter(author=request.user.profile.username)
+    posts = Post.objects.filter(author=request.user)
     # Get notifications for current user
     notifications = Notification.objects.filter(user=request.user).order_by('-timestamp')[:10]
     unread_notifications = Notification.objects.filter(user=request.user, is_read=False)
@@ -423,10 +423,10 @@ def show_profile(request, username):
             following = Follow.objects.filter(follower=request.user, following=user)
             if following.exists():
                 is_following = True  
-            posts = Post.objects.filter(author=user.profile.useraname)    
+            posts = Post.objects.filter(author=user.profile)    
             return render(request, 'public_profile.html', {'user': user, 'posts': posts, 'is_following': is_following, 'notifications': notifications, 'unread_notifications': unread_notifications, 'new_conversation_id': new_conversation_id, 'LANGUAGES': settings.LANGUAGES})
     else:  # user is not authenticated
-        posts = Post.objects.filter(author=user.profile.username)
+        posts = Post.objects.filter(author=user.profile)
         return render(request, 'public_profile.html', {'user': user, 'posts': posts, 'is_following': is_following, 'notifications': notifications, 'unread_notifications': unread_notifications, 'new_conversation_id': new_conversation_id, 'LANGUAGES': settings.LANGUAGES})
 
 @login_required
