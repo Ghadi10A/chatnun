187661a8d9a28a49a80cb54a7f7dcd8e46f4e38f
ghadi10 <72098445+ghadi00@users.noreply.github.com>
2023-06-06 18:58:55 +0100

    Update settings.py

--- predictMarkets/settings.py
+++ predictMarkets/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages', 
     'django.contrib.staticfiles',
     'django.contrib.sitemaps',
+    'django_extensions',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
