2c148f946cd072dbc0b1aae8855ca123146fcd37
ghadi10 <72098445+ghadi00@users.noreply.github.com>
2023-06-06 17:35:32 +0100

    Create sitemaps.py

---
+++ myapp/sitemaps.py
@@ -0,0 +1,11 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+
+class MySitemap(Sitemap):
+    def items(self):
+        # Return a queryset of objects to include in the sitemap
+        return MyModel.objects.all()
+
+    def location(self, item):
+        # Return the URL for each object
+        return reverse('myapp:detail', args=[item.pk])
