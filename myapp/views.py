ec90f7d707fa1d73dc7bdc5ff840c0385f91af75
ghadi10 <72098445+ghadi00@users.noreply.github.com>
2023-06-07 17:58:01 +0100

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -215,11 +215,9 @@ def predict_signals(request):
     return render(request, 'predict_signal.html', {'form': form, 'new_conversation_id': new_conversation_id, 'LANGUAGES': settings.LANGUAGES})
 def generate_verification_link(request, user):
     current_site = get_current_site(request)
-    uidb64 = urlsafe_base64_encode(force_bytes(user.pk)).decode()
+    uid = urlsafe_base64_encode(force_bytes(user.pk))
     token = default_token_generator.make_token(user)
-    verification_link = f"{request.scheme}://{current_site.domain}/activate/{uidb64}/{token}/"
-    return verification_link
-
+    return f"{request.scheme}://{current_site.domain}/activate/{uid}/{token}/"
 # def send_verification_email(request, user):
 #     user = request.user
 #     verification_link = generate_verification_link(request, user)
