7bde2c41e51fe8543fac9debfde6f5bd42545043
ghadi10 <72098445+ghadi00@users.noreply.github.com>
2023-06-07 12:35:28 +0100

    Update admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -2,19 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User, Profile, Post, EmojiReaction, ChatHistory, Follow, Notification, Message, Group, GroupPost, Invitation, GroupMessage, Notification, Comment, EmojiReaction, ChatHistory
 
-class UserAdminCustom(UserAdmin):
-    fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',
-                                    'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
-        ('Subscription', {'fields': ('profile.subscription_plan', 'profile.subscription_end',
-                                     'profile.subscription_status')}),
-    )
-
-admin.site.unregister(User)
-admin.site.register(User, UserAdminCustom)
+admin.site.register(User)
 admin.site.register(Profile)
 admin.site.register(Post)
 admin.site.register(Follow)
