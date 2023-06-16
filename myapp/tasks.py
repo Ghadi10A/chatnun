4f5ea94d51cc0689d6a22b650141ccd2983a4c61
ghadi10 <72098445+ghadi00@users.noreply.github.com>
2023-06-16 17:35:41 +0100

    Update tasks.py

--- myapp/tasks.py
+++ myapp/tasks.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 def scanner(request, interval=''):
     # Set the list of tickers you want to scan
     tickers = [
-        'GOLD', 'OIL', 'NDX', 'AAPL', 'GOOG', 'AMZN', 'TSLA', 'EURUSD', 'USDGBP', 'USDAUD', 'USDNZD', 'EURJPY', 'GBPJPY', 'EURGBP',
+        'MCX:GOLD1!', 'AMEX:OIL', 'NDX', 'AAPL', 'GOOG', 'AMZN', 'TSLA', 'EURUSD', 'USDGBP', 'USDAUD', 'USDNZD', 'EURJPY', 'GBPJPY', 'EURGBP',
         ]
 
     # Initialize an empty list to store the results for each ticker
