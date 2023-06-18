5ad4b915d96cee9a2e9a3f2f73bc28a8664cafdf
ghadi10 <72098445+ghadi00@users.noreply.github.com>
2023-06-18 12:20:50 +0100

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -703,7 +703,13 @@ def home(request, post_id=None):
         notification.is_read = True
         notification.save()      
     new_conversation_id = str(uuid.uuid4())
-    users = User.objects.select_related('profile').all()
+    users = User.objects.all()
+    current_user = request.user
+
+    for user in users:
+        if user != current_user and user.is_authenticated:
+            user.is_connected = True
+            user.save()
     return render(request, 'home.html', {'form_post': form_post, 'search_form': search_form, 'form_comment': form_comment, 'form_reaction': form_reaction, 'posts': posts, 'reactions': reactions, 'notifications': notifications, 'unread_notifications': unread_notifications, 'message_notifications': message_notifications, 'post_notifications': post_notifications, 'group_message_notifications': group_message_notifications, 'group_post_notifications': group_post_notifications, 'comments': comments, 'emoji_reactions': emoji_reactions, 'new_conversation_id': new_conversation_id, 'LANGUAGES': settings.LANGUAGES, 'users': users}) 
 
 @login_required(login_url='get_started')
