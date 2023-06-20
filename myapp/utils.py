d24a2c2ab76fbc0ac72061205905d9600072fb01
ghadiamine <72098445+Ghadi10A@users.noreply.github.com>
2023-06-20 16:39:25 +0100

    Update utils.py

--- myapp/utils.py
+++ myapp/utils.py
@@ -104,12 +104,11 @@ def predict_signal(ticker):
     data['VWAP'] = (data['Close'] * data['Volume']).cumsum() / data['Volume'].cumsum()
 
     # Pre-process the latest data using the loaded scaler
-    scaled_data = scaler.transform(data[['Open', 'High', 'Low', 'Close', 'Volume', 'VWAP']])
+    scaled_data = scaler.fit_transform(data[['Open', 'High', 'Low', 'Close', 'Volume', 'VWAP']])
     data[['Open', 'High', 'Low', 'Close', 'Volume', 'VWAP']] = scaled_data
 
     # Define the target variable
-    prediction = model.predict(data[['Open', 'High', 'Low', 'Close', 'Volume', 'VWAP']])
-
+    prediction = np.where(data['Close'].shift(-1) > data['Close'], 1, 0)
     # Determine the position based on the trend and the prediction
     if np.any(prediction == 1):
         signal = 'Buy'
@@ -117,6 +116,7 @@ def predict_signal(ticker):
         signal = 'Sell'
     else:
         signal = 'Neutral'
+
 
     # Calculate other metrics
     last_diff = data['Close'][-1] - data['Close'][-2]
