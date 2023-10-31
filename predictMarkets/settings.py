a88cfe3054b9ea88e81b913c75642ab6e2c56910
ghadiamine <72098445+Ghadi10A@users.noreply.github.com>
2023-10-31 13:16:55 +0100

    Update settings.py

--- predictMarkets/settings.py
+++ predictMarkets/settings.py
@@ -31,10 +31,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 #DEBUG = config('DEBUG', cast=bool)
-#PREPEND_WWW = True
-#BASE_URL = "www.chatnun.com"
+PREPEND_WWW = True
+BASE_URL = "www.chatnun.com"
 ALLOWED_HOSTS = ['predict-markets-4c338fe7cb05.herokuapp.com', '66.96.162.150', 'chatnun.com', 'www.chatnun.com']
 
 
